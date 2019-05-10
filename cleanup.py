e8b96af59fab1668298652e89da3f2e87b43766a
Periklis Gkolias <PeriGK@users.noreply.github.com>
2019-05-11 00:10:01 +0300

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -1,1 +1,24 @@
+import os
+
+# Example function, not uninstall packages with the 
+# digram py in their name. Please update accordingly
+def filter_function(name):
+    return 'py' not in name
+
+conda_list_output = os.popen('conda list').read()
+
+output_in_lines = conda_list_output.split('\n')
+
+# Line 4 and onwards is what we want
+
+output_in_lines = output_in_lines[3:]
+
+# Name appears in the beginning of the line
+only_names = [line.strip().split(' ')[0] for line in output_in_lines]
+
+filtered_list = list(filter(filter_function, only_names))
+
+cmd = f'conda uninstall {" ".join(filtered_list)}'
+
+os.system(cmd)
 
